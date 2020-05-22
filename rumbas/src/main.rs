222549a7b4569bb2d239dec9f0e3b05bae32c88c
Jesse Hoobergs <jesse@hoobergs.be>
2020-05-22 14:34:22 +0200

    Check empty fields on convert to numbas: NumbasResult type

--- rumbas/src/main.rs
+++ rumbas/src/main.rs
@@ -1,5 +1,35 @@
+use std::env;
 mod data;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => println!("Please provide an argument"),
+        2 => {
+            let path = &args[1];
+
+            let exam = data::exam::Exam::from_file(path);
+            match exam {
+                Ok(v) => {
+                    println!("{:#?}", v);
+                    let numbas = v.to_numbas();
+                    match numbas {
+                        Ok(res) => println!("{:#?}", res),
+                        Err(missing_fields) => {
+                            println!("Missing fields:\n{}", missing_fields.join("\n"))
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!(
+                        "Error in the json on column {} of line {}. The type of the error is {:?}",
+                        e.column(),
+                        e.line(),
+                        e.classify() // Better explanation: Eof -> end of file, Data: wrong datatype or missing field, Syntax: syntax error
+                    );
+                }
+            };
+        }
+        _ => println!("Too many arguments"),
+    }
 }
