2dde82ca24382f83a99949463386202e0ce182b1
Jesse Hoobergs <jesse@hoobergs.be>
2020-06-10 16:56:28 +0200

    Add substitution to translatable strings

--- rumbas/src/data/translatable.rs
+++ rumbas/src/data/translatable.rs
@@ -6,7 +6,8 @@ use std::collections::HashMap;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(untagged)]
 pub enum TranslatableString {
-    Translation(HashMap<String, String>),
+    //TODO: custom reader that checks for missing values etc?
+    Translated(HashMap<String, String>), // Maps locales on formattable strings and parts like "{func}" (between {}) to values
     NotTranslated(String),
 }
 
@@ -27,7 +28,67 @@ impl TranslatableString {
     pub fn to_string(&self, locale: &String) -> Option<String> {
         match self {
             TranslatableString::NotTranslated(s) => Some(s.clone()),
-            TranslatableString::Translation(m) => m.get(locale).map(|s| s.clone()),
+            TranslatableString::Translated(m) => m.get(locale).map(|s| substitute(s, &m)),
         }
+    }
+}
+
+fn substitute(pattern: &String, map: &HashMap<String, String>) -> String {
+    let mut result = pattern.clone();
+    for (key, val) in map.iter() {
+        if key.starts_with("{") && key.ends_with("}") {
+            result = result.replace(key, val);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn no_translation() {
+        let val = "some string".to_string();
+        let t = TranslatableString::NotTranslated(val.clone());
+        assert_eq!(t.to_string(&"any locale".to_string()), Some(val));
+    }
+
+    #[test]
+    fn simple_translation() {
+        let val_nl = "een string".to_string();
+        let val_en = "some string".to_string();
+        let mut m = HashMap::new();
+        m.insert("nl".to_string(), val_nl.clone());
+        m.insert("en".to_string(), val_en.clone());
+        let t = TranslatableString::Translated(m);
+        assert_eq!(t.to_string(&"nl".to_string()), Some(val_nl));
+        assert_eq!(t.to_string(&"en".to_string()), Some(val_en));
+    }
+
+    #[test]
+    fn substitution_translation() {
+        let val_nl = "een string met functie {func} en {0}".to_string();
+        let val_en = "some string with function {func} and {0}".to_string();
+        let mut m = HashMap::new();
+        m.insert("nl".to_string(), val_nl.clone());
+        m.insert("en".to_string(), val_en.clone());
+        m.insert("{0}".to_string(), "x^2".to_string());
+        m.insert("{func}".to_string(), "e^x".to_string());
+        let t = TranslatableString::Translated(m.clone());
+        assert_eq!(
+            t.to_string(&"nl".to_string()),
+            Some(format!(
+                "een string met functie {} en {}",
+                m["{func}"], m["{0}"]
+            ))
+        );
+        assert_eq!(
+            t.to_string(&"en".to_string()),
+            Some(format!(
+                "some string with function {} and {}",
+                m["{func}"], m["{0}"]
+            ))
+        );
     }
 }
