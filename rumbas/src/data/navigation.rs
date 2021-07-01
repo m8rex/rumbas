ae922f348c17eeac468a32facb92dd272365a3ef
Jesse Hoobergs <jesse@codebergs.com>
2021-07-01 22:44:43 +0200

    Begin

--- rumbas/src/data/navigation.rs
+++ rumbas/src/data/navigation.rs
@@ -6,37 +6,170 @@ use crate::data::translatable::TranslatableString;
 use serde::{Deserialize, Serialize};
 
 optional_overwrite! {
-    pub struct Navigation {
-        allow_regenerate: bool,
-        reverse: bool,
-        browsing_enabled: bool,
-        allow_steps: bool,
-        show_frontpage: bool,
-        show_results_page: ShowResultsPage,
-        prevent_leaving: bool,
-        on_leave: LeaveAction,
+    pub struct NavigationSharedData {
+        /// Password to begin the exam
         start_password: FileString, //TODO: Noneable, but "" is none in this case?
+        /// Whether the student can regenerate questions (alias of `can_regenerate`)
+        #[serde(alias = "allow_regenerate")]
+        can_regenerate: bool,
+        /// If false,  then part steps will not be offered to the student, regardless of whether any have been defined in the exam’s questions
+        #[serde(alias = "allow_steps")]
+        show_steps: bool,
+        /// Whether the title page should be shown.
+        #[serde(alias = "show_frontpage")]
+        show_title_page: bool,
+        /// Whether the student will be asked to confirm when leaving the exam.
+        prevent_leaving: bool,
         show_names_of_question_groups: bool
     }
 }
 
-impl ToNumbas for Navigation {
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "mode")]
+pub enum NormalNavigation {
+    Sequential(SequentialNavigation),
+    Menu(MenuNavigation),
+}
+impl_optional_overwrite!(NormalNavigation);
+
+impl NormalNavigation {
+    pub fn to_shared_data(&self) -> NavigationSharedData {
+        match self {
+            NormalNavigation::Menu(m) => m.shared_data.clone().unwrap(),
+            NormalNavigation::Sequential(m) => m.shared_data.clone().unwrap(),
+        }
+    }
+    pub fn can_move_to_previous(&self) -> Option<bool> {
+        match self {
+            NormalNavigation::Menu(m) => Some(false),
+            NormalNavigation::Sequential(m) => Some(m.can_move_to_previous.clone().unwrap()),
+        }
+    }
+    pub fn to_navigation_mode(&self) -> numbas::exam::ExamNavigationMode {
+        match self {
+            NormalNavigation::Menu(m) => numbas::exam::ExamNavigationMode::Menu,
+            NormalNavigation::Sequential(m) => numbas::exam::ExamNavigationMode::Sequence,
+        }
+    }
+    pub fn browsing_enabled(&self) -> bool {
+        match self {
+            NormalNavigation::Menu(m) => false,
+            NormalNavigation::Sequential(m) => m.browsing_enabled.clone().unwrap(),
+        }
+    }
+    pub fn show_results_page(&self) -> Option<ShowResultsPage> {
+        match self {
+            NormalNavigation::Menu(m) => None,
+            NormalNavigation::Sequential(m) => Some(m.show_results_page.clone().unwrap()),
+        }
+    }
+    pub fn on_leave(&self) -> Option<LeaveAction> {
+        match self {
+            NormalNavigation::Menu(m) => None,
+            NormalNavigation::Sequential(m) => Some(m.on_leave.clone().unwrap()),
+        }
+    }
+}
+
+optional_overwrite! {
+    pub struct SequentialNavigation {
+        /// (flattened field) The data shared between all types of navigation
+        #[serde(flatten)]
+        shared_data: NavigationSharedData,
+        /// Whether the student can move back to previous question (alias of `reverse`)
+        #[serde(alias = "reverse")]
+        can_move_to_previous: bool,
+        /// Whether the student can jump to any question.
+        browsing_enabled: bool,
+        /// When the results page should be shown
+        show_results_page: ShowResultsPage,
+        /// Action to execute when a student changes question or tries to end the exam.
+        on_leave: LeaveAction
+    }
+}
+
+optional_overwrite! {
+    pub struct MenuNavigation {
+        /// (flattened field) The data shared between all types of navigation
+        #[serde(flatten)]
+        shared_data: NavigationSharedData
+    }
+}
+
+impl ToNumbas for NormalNavigation {
     type NumbasType = numbas::exam::ExamNavigation;
     fn to_numbas(&self, locale: &String) -> NumbasResult<numbas::exam::ExamNavigation> {
         let empty_fields = self.empty_fields();
         if empty_fields.is_empty() {
             Ok(numbas::exam::ExamNavigation::new(
-                self.allow_regenerate.unwrap(),
-                Some(self.reverse.clone().unwrap()),
-                Some(self.browsing_enabled.clone().unwrap()),
-                Some(self.allow_steps.clone().unwrap()),
-                self.show_frontpage.unwrap(),
-                self.show_results_page
+                self.to_shared_data().can_regenerate.unwrap(),
+                self.to_navigation_mode(),
+                self.can_move_to_previous(),
+                Some(self.browsing_enabled()),
+                Some(self.to_shared_data().show_steps.clone().unwrap()),
+                self.to_shared_data().show_title_page.clone().unwrap(),
+                self.show_results_page() // TODO
                     .clone()
                     .map(|s| s.to_numbas(&locale).unwrap()),
-                Some(self.prevent_leaving.clone().unwrap()),
+                Some(self.to_shared_data().prevent_leaving.clone().unwrap()),
+                self.on_leave()
+                    .clone()
+                    .map(|s| s.to_numbas(&locale).unwrap()),
+                self.to_shared_data()
+                    .start_password
+                    .clone()
+                    .map(|s| s.get_content(&locale)),
+            ))
+        } else {
+            Err(empty_fields)
+        }
+    }
+}
+
+optional_overwrite! {
+    pub struct DiagnosticNavigation {
+        /// (flattened field) The data shared between all types of navigation
+        #[serde(flatten)]
+        shared_data: NavigationSharedData,
+        /// Action to execute when a student changes question or tries to end the exam.
+        on_leave: LeaveAction
+    }
+}
+
+impl ToNumbas for DiagnosticNavigation {
+    type NumbasType = numbas::exam::ExamNavigation;
+    fn to_numbas(&self, locale: &String) -> NumbasResult<numbas::exam::ExamNavigation> {
+        let empty_fields = self.empty_fields();
+        if empty_fields.is_empty() {
+            Ok(numbas::exam::ExamNavigation::new(
+                self.shared_data.clone().unwrap().can_regenerate.unwrap(),
+                numbas::exam::ExamNavigationMode::Diagnostic,
+                None,
+                None,
+                Some(self.shared_data.clone().unwrap().show_steps.unwrap()),
+                self.shared_data
+                    .clone()
+                    .unwrap()
+                    .show_title_page
+                    .clone()
+                    .unwrap(),
+                None,
+                Some(
+                    self.shared_data
+                        .clone()
+                        .unwrap()
+                        .prevent_leaving
+                        .clone()
+                        .unwrap(),
+                ),
                 self.on_leave.clone().map(|s| s.to_numbas(&locale).unwrap()),
-                self.start_password.clone().map(|s| s.get_content(&locale)),
+                self.shared_data
+                    .clone()
+                    .unwrap()
+                    .start_password
+                    .clone()
+                    .map(|s| s.get_content(&locale)),
             ))
         } else {
             Err(empty_fields)
---
+++ rumbas/src/data/normal_exam.rs
@@ -0,0 +1,136 @@
+use crate::data::extension::Extensions;
+use crate::data::feedback::Feedback;
+use crate::data::locale::Locale;
+use crate::data::navigation::NormalNavigation;
+use crate::data::numbas_settings::NumbasSettings;
+use crate::data::optional_overwrite::{Noneable, OptionalOverwrite};
+use crate::data::question_group::QuestionGroup;
+use crate::data::template::{Value, ValueType, TEMPLATE_EXAMS_FOLDER};
+use crate::data::timing::Timing;
+use crate::data::to_numbas::{NumbasResult, ToNumbas};
+use crate::data::translatable::TranslatableString;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+optional_overwrite! {
+    /// An Exam
+    pub struct NormalExam {
+        /// All locales for which the exam should be generated
+        locales: Vec<Value<Locale>>,
+        /// The name of the exam
+        name: TranslatableString,
+        /// The navigation settings for this exam
+        navigation: NormalNavigation,
+        /// The timing settings for this exam
+        timing: Timing,
+        /// The feedback settings for this exam
+        feedback: Feedback,
+        /// The questions groups for this exam
+        question_groups: Vec<Value<QuestionGroup>>, //TODO: remove?
+        /// The settings to set for numbas
+        numbas_settings: NumbasSettings
+    }
+}
+
+impl ToNumbas for NormalExam {
+    type NumbasType = numbas::exam::Exam;
+    fn to_numbas(&self, locale: &String) -> NumbasResult<numbas::exam::Exam> {
+        let empty_fields = self.empty_fields();
+        if empty_fields.is_empty() {
+            let basic_settings = numbas::exam::BasicExamSettings::new(
+                self.name.clone().unwrap().to_string(locale).unwrap(), //TODO: might fail, not checked
+                self.timing
+                    .clone()
+                    .unwrap()
+                    .duration_in_seconds
+                    .to_numbas(&locale)
+                    .unwrap(),
+                self.feedback
+                    .clone()
+                    .unwrap()
+                    .percentage_needed_to_pass
+                    .to_numbas(&locale)
+                    .unwrap(),
+                Some(
+                    self.navigation
+                        .clone()
+                        .unwrap()
+                        .to_shared_data()
+                        .show_names_of_question_groups
+                        .unwrap(),
+                ),
+                Some(self.feedback.clone().unwrap().show_name_of_student.unwrap()),
+                Some(
+                    self.numbas_settings
+                        .clone()
+                        .unwrap()
+                        .allow_printing
+                        .unwrap(),
+                ),
+            );
+
+            //TODO
+            let navigation = self.navigation.clone().unwrap().to_numbas(&locale).unwrap();
+
+            //TODO
+            let timing = self.timing.clone().unwrap().to_numbas(&locale).unwrap();
+
+            //TODO
+            let feedback = self.feedback.clone().unwrap().to_numbas(&locale).unwrap();
+
+            //TODO
+            let functions = Value::Normal(HashMap::new());
+
+            //TODO
+            let variables = Value::Normal(HashMap::new());
+
+            //TODO
+            let question_groups: Vec<numbas::exam::ExamQuestionGroup> = self
+                .question_groups
+                .clone()
+                .unwrap()
+                .iter()
+                .map(|qg| qg.clone().to_numbas(&locale).unwrap())
+                .collect();
+
+            // Below from questions
+            //TODO
+            let resources: Vec<[String; 2]> = Vec::new();
+
+            let extensions: Vec<String> = self
+                .question_groups
+                .clone()
+                .unwrap()
+                .iter()
+                .flat_map(|qg| {
+                    qg.clone()
+                        .unwrap()
+                        .questions
+                        .unwrap()
+                        .into_iter()
+                        .map(|q| q.unwrap().question_data.unwrap().extensions.unwrap())
+                })
+                .fold(Extensions::new(), |a, b| Extensions::combine(a, b))
+                .to_paths();
+
+            //TODO
+            let custom_part_types: Vec<numbas::exam::CustomPartType> = Vec::new();
+
+            Ok(numbas::exam::Exam::new(
+                basic_settings,
+                resources,
+                extensions,
+                custom_part_types,
+                navigation,
+                timing,
+                feedback,
+                Some(functions.unwrap()),
+                Some(variables.unwrap()),
+                question_groups,
+                None,
+            ))
+        } else {
+            Err(empty_fields)
+        }
+    }
+}
