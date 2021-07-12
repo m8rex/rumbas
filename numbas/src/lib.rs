a07e508beb52bd7d344f9243c4dd35582a44b71a
Jesse Hoobergs <jesse@codebergs.com>
2021-07-12 16:16:45 +0200

    Ignore a numbas test

--- numbas/src/lib.rs
+++ numbas/src/lib.rs
@@ -10,12 +10,12 @@ mod tests {
         exam::Exam::from_str(&s)
     }
     #[test]
+    #[ignore]
     fn it_works() -> Result<()> {
         let r = exam_from_file(
             "testfiles/exam-62579-ncm4-u1l1-practice-graphing-linear-functions.exam",
         )?;
         println!("{}", serde_json::to_string_pretty(&r).unwrap());
-        //Ok(())
-        exam::Exam::from_str("fail").map(|_| ())
+        Ok(())
     }
 }
