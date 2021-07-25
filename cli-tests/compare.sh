5cc1f1d2d51f138bb30ea0743a4f6c47656d8a6b
Jesse Hoobergs <jesse@codebergs.com>
2021-07-25 20:03:37 +0200

    Add script to compare exam file given to rumbas and the one created by rumbas

---
+++ cli-tests/compare.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+jq_in_place(){
+  COMMAND=$1
+  FILE=$2
+  jq $COMMAND $FILE > "$FILE.tmp"
+  mv "$FILE.tmp" $FILE
+}
+
+clean(){
+  FILE=$1
+  NEW_FILE_NAME=$2
+  tail -n+2 "$FILE" > $NEW_FILE_NAME
+  jq_in_place 'del(.question_groups[].questions[].contributors)' $NEW_FILE_NAME
+  jq_in_place 'del(.contributors)' $NEW_FILE_NAME
+  jq_in_place 'del(.question_groups[].questions[].metadata)' $NEW_FILE_NAME
+  jq_in_place 'del(.metadata)' $NEW_FILE_NAME
+  jq_in_place '.question_groups[].questions[].extensions[]|=sub("'$NUMBAS_FOLDER'/extensions/";"")' $NEW_FILE_NAME
+  jq_in_place '.extensions[]|=sub("'$NUMBAS_FOLDER'/extensions/";"")' $NEW_FILE_NAME
+  jq_in_place '.resources[][1]|=sub(".*/question-resources/";"")' $NEW_FILE_NAME
+  jq_in_place '.question_groups[].questions[].resources[][1]|=sub(".*/question-resources/";"")' $NEW_FILE_NAME
+}
+
+FILE_NUMBAS="tmp/"$3"-numbas.json"
+FILE_RUMBAS="tmp/"$3"-rumbas.json"
+
+clean "$1" $FILE_NUMBAS
+clean "$2" $FILE_RUMBAS
+
+jd $FILE_NUMBAS $FILE_RUMBAS > "tmp/"$3".patch"
