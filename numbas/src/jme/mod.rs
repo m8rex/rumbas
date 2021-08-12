c430165e3ad01cccb5a2be88fedb005a1ac6698b
Jesse Hoobergs <jesse@codebergs.com>
2021-08-12 16:56:38 +0200

    Add ContentAreaString

--- numbas/src/jme/mod.rs
+++ numbas/src/jme/mod.rs
@@ -67,3 +67,50 @@ impl std::convert::From<EmbracedJMEString> for String {
         jme.s
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[serde(try_from = "String")]
+#[serde(into = "String")]
+pub struct ContentAreaString {
+    s: String,
+    asts: Option<Vec<ast::Expr>>,
+}
+
+impl std::convert::TryFrom<String> for ContentAreaString {
+    type Error = String; // TODO
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        let trimmed = s.trim();
+        let asts = if trimmed.is_empty() {
+            None
+        } else {
+            let pairs = parser::parse_as_embraced_jme(&trimmed).map_err(|e| format!("{:?}", e))?;
+            let asts = parser::consume_expressions(pairs).map_err(|e| format!("{:?}", e))?;
+            Some(asts)
+        };
+        Ok(Self {
+            s: trimmed.to_owned(),
+            asts,
+        })
+    }
+}
+
+impl std::convert::From<ContentAreaString> for String {
+    fn from(jme: ContentAreaString) -> Self {
+        jme.s
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::convert::TryFrom;
+    #[test]
+    fn content_area() {
+        let res = ContentAreaString::try_from(
+            "<p>Bob the farmer has {num_animals} {animal_name}</p>".to_string(),
+        );
+        assert!(res.is_ok());
+        assert!(res.as_ref().unwrap().asts.is_some());
+        assert_eq!(res.unwrap().asts.unwrap().len(), 2);
+    }
+}
