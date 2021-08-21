f9f9cbd0f4f780564b5adb389034f067a1865500
Jesse Hoobergs <jesse@codebergs.com>
2021-08-21 13:40:43 +0200

    Cleanup support/yaml.rs & support/default.rs

--- rumbas/src/support/yaml.rs
+++ rumbas/src/support/yaml.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+pub type YamlResult<T> = Result<T, YamlError>;
+
 #[derive(Debug)]
 pub struct YamlError {
     error: serde_yaml::Error,
@@ -33,5 +35,3 @@ impl std::fmt::Display for YamlError {
         }
     }
 }
-
-pub type YamlResult<T> = Result<T, YamlError>;
