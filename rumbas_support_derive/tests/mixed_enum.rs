cde6ce8eb420ff39b89ed930c2b4bbe3c5c277df
Jesse Hoobergs <jesse@codebergs.com>
2021-09-01 16:54:26 +0200

    Some more tests

--- rumbas_support_derive/tests/mixed_enum.rs
+++ rumbas_support_derive/tests/mixed_enum.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rumbas_support_derive;
 
+include! {"macros.rs.include"}
+
 use rumbas_support::input::Input;
 use rumbas_support::input::InputCheckResult;
 use rumbas_support::input::InputInverse;
@@ -33,88 +35,58 @@ pub enum TestOverwrite {
     Struct { field1: bool, field2: f64 },
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use rumbas_support::input::Input;
+#[test]
+fn create_test2() {
+    let _test2 = Test2 {
+        field1: vec![Test::Unit],
+        field2: 65.0,
+    };
 
-    #[test]
-    fn create_test2() {
-        let _test2 = Test2 {
-            field1: vec![Test::Unit],
-            field2: 65.0,
-        };
+    let _test2 = Test2Input {
+        field1: Value::Normal(vec![TestInput::Struct {
+            a: Value::Normal(5.8),
+        }]),
+        field2: Value::Normal(65.0),
+    };
+}
 
-        let _test2 = Test2Input {
-            field1: Value::Normal(vec![TestInput::Struct {
-                a: Value::Normal(5.8),
-            }]),
-            field2: Value::Normal(65.0),
-        };
-    }
+#[test]
+fn find_missing() {
+    assert_no_missing!(TestInput::Unit);
 
-    macro_rules! assert_no_missing {
-        ($expr: expr) => {
-            let item = $expr;
-            let missing = item.find_missing();
-            assert!(missing.is_empty());
-        };
-    }
-
-    macro_rules! assert_missing_fields {
-        ($expr: expr, $fields: expr) => {
-            let item = $expr;
-            let missing = item.find_missing();
-            assert_eq!(
-                missing
-                    .missing_fields()
-                    .iter()
-                    .map(|p| p.to_string())
-                    .collect::<Vec<_>>(),
-                $fields
-            );
-            assert!(missing.invalid_yaml_fields().is_empty());
-        };
-    }
-
-    #[test]
-    fn find_missing() {
-        assert_no_missing!(TestInput::Unit);
-
-        assert_no_missing!(TestInput::Tuple(
-            Value::Normal(5.8),
+    assert_no_missing!(TestInput::Tuple(
+        Value::Normal(5.8),
+        Value::Normal(true),
+        Value::Normal("s".to_owned())
+    ));
+    assert_missing_fields!(
+        TestInput::Tuple(
+            Value::None(),
             Value::Normal(true),
             Value::Normal("s".to_owned())
-        ));
-        assert_missing_fields!(
-            TestInput::Tuple(
-                Value::None(),
-                Value::Normal(true),
-                Value::Normal("s".to_owned())
-            ),
-            vec!["0"] // TODO
-        );
-        assert_missing_fields!(
-            TestInput::Tuple(
-                Value::Normal(5.8),
-                Value::None(),
-                Value::Normal("s".to_owned())
-            ),
-            vec!["1"]
-        );
-        assert_missing_fields!(
-            TestInput::Tuple(Value::Normal(5.8), Value::Normal(true), Value::None()),
-            vec!["2"]
-        );
-        assert_missing_fields!(
-            TestInput::Tuple(Value::None(), Value::Normal(true), Value::None()),
-            vec!["0", "2"]
-        );
+        ),
+        vec!["0"] // TODO
+    );
+    assert_missing_fields!(
+        TestInput::Tuple(
+            Value::Normal(5.8),
+            Value::None(),
+            Value::Normal("s".to_owned())
+        ),
+        vec!["1"]
+    );
+    assert_missing_fields!(
+        TestInput::Tuple(Value::Normal(5.8), Value::Normal(true), Value::None()),
+        vec!["2"]
+    );
+    assert_missing_fields!(
+        TestInput::Tuple(Value::None(), Value::Normal(true), Value::None()),
+        vec!["0", "2"]
+    );
 
-        assert_no_missing!(TestInput::Struct {
-            a: Value::Normal(5.8)
-        });
+    assert_no_missing!(TestInput::Struct {
+        a: Value::Normal(5.8)
+    });
 
-        assert_missing_fields!(TestInput::Struct { a: Value::None() }, vec!["a"]);
-    }
+    assert_missing_fields!(TestInput::Struct { a: Value::None() }, vec!["a"]);
 }
--- rumbas_support_derive/tests/overwrite.rs
+++ rumbas_support_derive/tests/overwrite.rs
@@ -1,252 +1,199 @@
 #[macro_use]
 extern crate rumbas_support_derive;
 
-#[cfg(test)]
-mod test {
+include! {"macros.rs.include"}
 
-    use rumbas_support::input::Input;
-    use rumbas_support::input::InputCheckResult;
-    use rumbas_support::input::InputInverse;
-    use rumbas_support::overwrite::Overwrite;
-    use rumbas_support::value::Value;
-    use serde::Deserialize;
+use rumbas_support::input::Input;
+use rumbas_support::input::InputCheckResult;
+use rumbas_support::input::InputInverse;
+use rumbas_support::overwrite::Overwrite;
+use rumbas_support::value::Value;
+use serde::Deserialize;
 
-    #[derive(Debug, PartialEq, Deserialize, Input, Overwrite)]
-    #[input(name = "TempInput")]
-    pub struct Temp {
-        name: String,
-        test: String,
-    }
+#[derive(Debug, PartialEq, Deserialize, Input, Overwrite)]
+#[input(name = "TempInput")]
+pub struct Temp {
+    name: String,
+    test: String,
+}
 
-    #[derive(Debug, PartialEq, Deserialize, Input, Overwrite)]
-    #[input(name = "Temp2Input")]
-    pub struct Temp2 {
-        other: String,
-        t: Temp,
-    }
-    //TODO: template
-    #[test]
-    fn check_simple_structs() {
-        let t = TempInput {
-            name: Value::Normal("test".to_string()),
-            test: Value::None(),
-        };
-        let check = t.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["test"],
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-        let t = TempInput {
-            name: Value::Normal("test2".to_string()),
+#[derive(Debug, PartialEq, Deserialize, Input, Overwrite)]
+#[input(name = "Temp2Input")]
+pub struct Temp2 {
+    other: String,
+    t: Temp,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Input, Overwrite)]
+#[input(name = "TempEnumInput")]
+pub enum TempEnum {
+    Unit,
+    Tuple(Temp, Temp2),
+    Struct { a: Temp, b: bool },
+}
+
+//TODO: template
+#[test]
+fn check_simple_structs() {
+    let t = TempInput {
+        name: Value::Normal("test".to_string()),
+        test: Value::None(),
+    };
+    assert_missing_fields!(t, vec!["test"]);
+    let t = TempInput {
+        name: Value::Normal("test2".to_string()),
+        test: Value::Normal("name".to_string()),
+    };
+    assert_no_missing!(t);
+    let t = TempInput {
+        name: Value::None(),
+        test: Value::None(),
+    };
+    assert_missing_fields!(t, vec!["name", "test"]);
+}
+
+#[test]
+fn check_complex_structs() {
+    let t = Temp2Input {
+        other: Value::Normal("val".to_string()),
+        t: Value::Normal(TempInput {
+            name: Value::Normal("val".to_string()),
             test: Value::Normal("name".to_string()),
-        };
-        assert_eq!(t.find_missing().is_empty(), true);
-        let t = TempInput {
+        }),
+    };
+    assert_no_missing!(t);
+    let t = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
+            name: Value::None(),
+            test: Value::Normal("name".to_string()),
+        }),
+    };
+    assert_missing_fields!(t, vec!["other", "t.name"]);
+    let t = Temp2Input {
+        other: Value::None(),
+        t: Value::None(),
+    };
+    assert_missing_fields!(t, vec!["other", "t"]);
+    let t = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
             name: Value::None(),
             test: Value::None(),
-        };
-        let check = t.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["name", "test"],
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-    }
+        }),
+    };
+    assert_missing_fields!(t, vec!["other", "t.name", "t.test"]);
+}
 
-    #[test]
-    fn check_complex_structs() {
-        let t = Temp2Input {
-            other: Value::Normal("val".to_string()),
-            t: Value::Normal(TempInput {
-                name: Value::Normal("val".to_string()),
-                test: Value::Normal("name".to_string()),
-            }),
-        };
-        assert_eq!(t.find_missing().is_empty(), true);
-        let t = Temp2Input {
-            other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::None(),
-                test: Value::Normal("name".to_string()),
-            }),
-        };
-        let check = t.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["other", "t.name"],
-        );
-        assert_eq!(check.invalid_yaml_fields().is_empty(), true);
-        let t = Temp2Input {
-            other: Value::None(),
-            t: Value::None(),
-        };
-        let check = t.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["other", "t"],
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-        let t = Temp2Input {
-            other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::None(),
-                test: Value::None(),
-            }),
-        };
-        let check = t.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["other", "t.name", "t.test"],
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-    }
+#[test]
+fn overwrite_simple_structs() {
+    let mut t = TempInput {
+        name: Value::Normal("test".to_string()),
+        test: Value::None(),
+    };
+    let t2 = TempInput {
+        name: Value::Normal("test2".to_string()),
+        test: Value::Normal("name".to_string()),
+    };
+    t.overwrite(&t2);
+    assert_eq!(
+        t,
+        TempInput {
+            name: t.clone().name,
+            test: t2.test,
+        }
+    );
+}
 
-    #[test]
-    fn overwrite_simple_structs() {
-        let mut t = TempInput {
-            name: Value::Normal("test".to_string()),
-            test: Value::None(),
-        };
-        let t2 = TempInput {
-            name: Value::Normal("test2".to_string()),
+#[test]
+fn overwrite_nested_structs() {
+    let t3 = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
+            name: Value::None(),
             test: Value::Normal("name".to_string()),
-        };
-        t.overwrite(&t2);
-        assert_eq!(
-            t,
-            TempInput {
-                name: t.clone().name,
-                test: t2.test,
-            }
-        );
-    }
-
-    #[test]
-    fn overwrite_nested_structs() {
-        let t3 = Temp2Input {
+        }),
+    };
+    let mut t4 = Temp2Input {
+        other: Value::None(),
+        t: Value::None(),
+    };
+    t4.overwrite(&t3);
+    assert_eq!(
+        t4,
+        Temp2Input {
             other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::None(),
-                test: Value::Normal("name".to_string()),
-            }),
-        };
-        let mut t4 = Temp2Input {
-            other: Value::None(),
-            t: Value::None(),
-        };
-        t4.overwrite(&t3);
-        assert_eq!(
-            t4,
-            Temp2Input {
-                other: Value::None(),
-                t: t3.clone().t
-            }
-        );
-        let t5 = Temp2Input {
-            other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::Normal("test".to_string()),
-                test: Value::Normal("name2".to_string()),
-            }),
-        };
-        t4.overwrite(&t5);
-        assert_eq!(
-            t4,
-            Temp2Input {
-                other: Value::None(),
-                t: Value::Normal(TempInput {
-                    name: t5.t.unwrap().name,
-                    test: t3.t.unwrap().test
-                }),
-            }
-        );
-    }
-
-    #[test]
-    fn check_vec_of_simple_structs() {
-        let t1 = TempInput {
+            t: t3.clone().t
+        }
+    );
+    let t5 = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
             name: Value::Normal("test".to_string()),
-            test: Value::None(),
-        };
-        let t2 = TempInput {
-            name: Value::Normal("test2".to_string()),
+            test: Value::Normal("name2".to_string()),
+        }),
+    };
+    t4.overwrite(&t5);
+    assert_eq!(
+        t4,
+        Temp2Input {
+            other: Value::None(),
+            t: Value::Normal(TempInput {
+                name: t5.t.unwrap().name,
+                test: t3.t.unwrap().test
+            }),
+        }
+    );
+}
+
+#[test]
+fn check_vec_of_simple_structs() {
+    let t1 = TempInput {
+        name: Value::Normal("test".to_string()),
+        test: Value::None(),
+    };
+    let t2 = TempInput {
+        name: Value::Normal("test2".to_string()),
+        test: Value::Normal("name".to_string()),
+    };
+    let t3 = TempInput {
+        name: Value::None(),
+        test: Value::None(),
+    };
+    let v = vec![t1, t2, t3];
+    assert_missing_fields!(v, vec!["0.test", "2.name", "2.test"]);
+}
+
+#[test]
+fn check_vec_ofcomplex_structs() {
+    let t1 = Temp2Input {
+        other: Value::Normal("val".to_string()),
+        t: Value::Normal(TempInput {
+            name: Value::Normal("val".to_string()),
             test: Value::Normal("name".to_string()),
-        };
-        let t3 = TempInput {
+        }),
+    };
+    let t2 = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
+            name: Value::None(),
+            test: Value::Normal("name".to_string()),
+        }),
+    };
+    let t3 = Temp2Input {
+        other: Value::None(),
+        t: Value::None(),
+    };
+    let t4 = Temp2Input {
+        other: Value::None(),
+        t: Value::Normal(TempInput {
             name: Value::None(),
             test: Value::None(),
-        };
-        let v = vec![t1, t2, t3];
-        let check = v.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["0.test", "2.name", "2.test"],
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-    }
-
-    #[test]
-    fn check_vec_ofcomplex_structs() {
-        let t1 = Temp2Input {
-            other: Value::Normal("val".to_string()),
-            t: Value::Normal(TempInput {
-                name: Value::Normal("val".to_string()),
-                test: Value::Normal("name".to_string()),
-            }),
-        };
-        let t2 = Temp2Input {
-            other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::None(),
-                test: Value::Normal("name".to_string()),
-            }),
-        };
-        let t3 = Temp2Input {
-            other: Value::None(),
-            t: Value::None(),
-        };
-        let t4 = Temp2Input {
-            other: Value::None(),
-            t: Value::Normal(TempInput {
-                name: Value::None(),
-                test: Value::None(),
-            }),
-        };
-        let v = vec![t1, t2, t3, t4];
-        let check = v.find_missing();
-        assert_eq!(
-            check
-                .missing_fields()
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>(),
-            vec!["1.other", "1.t.name", "2.other", "2.t", "3.other", "3.t.name", "3.t.test"]
-        );
-        assert_eq!(check.invalid_yaml_fields().len(), 0);
-    }
+        }),
+    };
+    let v = vec![t1, t2, t3, t4];
+    assert_missing_fields!(
+        v,
+        vec!["1.other", "1.t.name", "2.other", "2.t", "3.other", "3.t.name", "3.t.test"]
+    );
 }
