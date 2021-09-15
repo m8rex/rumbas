0b58ee653bd8a764ccda7393f95ac72128e9287a
Jesse Hoobergs <jhoobergs@users.noreply.github.com>
2021-09-15 15:23:27 +0200

    Add jme display options

--- numbas/src/question/answer_simplification.rs
+++ numbas/src/question/answer_simplification.rs
@@ -2,48 +2,30 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_with::skip_serializing_none;
+use std::convert::TryInto;
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+#[serde(try_from = "&str")]
+#[serde(into = "String")]
+pub enum AnswerSimplificationType {
+    Rule(AnswerSimplificationRule),
+    DisplayOption(AnswerSimplificationDisplayOption),
+}
 
 impl std::convert::TryFrom<&str> for AnswerSimplificationType {
-    type Error = &'static str;
+    type Error = String;
     fn try_from(whole_item: &str) -> Result<Self, Self::Error> {
-        let (item, value) = if whole_item.starts_with('!') {
-            let mut chars = whole_item.chars();
-            chars.next();
-            (chars.as_str(), false)
+        let rule: Result<AnswerSimplificationRule, _> = whole_item.clone().try_into();
+        if let Ok(r) = rule {
+            Ok(Self::Rule(r))
         } else {
-            (whole_item, true)
-        };
-        match item.to_lowercase().as_ref() {
-            "all" => Ok(AnswerSimplificationType::All(value)),
-            "basic" => Ok(AnswerSimplificationType::Basic(value)),
-            "unitfactor" => Ok(AnswerSimplificationType::UnitFactor(value)),
-            "unitpower" => Ok(AnswerSimplificationType::UnitPower(value)),
-            "unitdenominator" => Ok(AnswerSimplificationType::UnitDenominator(value)),
-            "zerofactor" => Ok(AnswerSimplificationType::ZeroFactor(value)),
-            "zeroterm" => Ok(AnswerSimplificationType::ZeroTerm(value)),
-            "zeropower" => Ok(AnswerSimplificationType::ZeroPower(value)),
-            "collectnumbers" => Ok(AnswerSimplificationType::CollectNumbers(value)),
-            "zerobase" => Ok(AnswerSimplificationType::ZeroBase(value)),
-            "constantsfirst" => Ok(AnswerSimplificationType::ConstantsFirst(value)),
-            "sqrtproduct" => Ok(AnswerSimplificationType::SqrtProduct(value)),
-            "sqrtfivision" => Ok(AnswerSimplificationType::SqrtDivision(value)),
-            "sqrtdquare" => Ok(AnswerSimplificationType::SqrtSquare(value)),
-            "othernumbers" => Ok(AnswerSimplificationType::OtherNumbers(value)),
-            "timesdot" => Ok(AnswerSimplificationType::TimesDot(value)),
-            "expandbrackets" => Ok(AnswerSimplificationType::ExpandBrackets(value)),
-            "noleadingminus" => Ok(AnswerSimplificationType::NoLeadingMinus(value)),
-            "trig" => Ok(AnswerSimplificationType::Trigonometric(value)),
-            "collectlikefractions" => Ok(AnswerSimplificationType::CollectLikeFractions(value)),
-            "canonicalorder" => Ok(AnswerSimplificationType::CanonicalOrder(value)),
-            "cancelfactors" => Ok(AnswerSimplificationType::CancelFactors(value)),
-            "cancelterms" => Ok(AnswerSimplificationType::CancelTerms(value)),
-            "simplifyfractions" => Ok(AnswerSimplificationType::Fractions(value)),
-            _ => {
-                /*       Err(serde::de::Error::custom(format!(
-                    "unknown answer simplification type {}",
-                    item
-                )))*/
-                Ok(AnswerSimplificationType::Unknown((item.to_string(), value)))
+            let display_option: Result<AnswerSimplificationDisplayOption, _> =
+                whole_item.try_into();
+            if let Ok(d) = display_option {
+                Ok(Self::DisplayOption(d))
+            } else {
+                Err(whole_item.to_string())
             }
         }
     }
@@ -51,66 +33,10 @@ impl std::convert::TryFrom<&str> for AnswerSimplificationType {
 
 impl std::fmt::Display for AnswerSimplificationType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                AnswerSimplificationType::All(true) => "all".to_string(),
-                AnswerSimplificationType::All(false) => "!all".to_string(),
-                AnswerSimplificationType::Basic(true) => "basic".to_string(),
-                AnswerSimplificationType::Basic(false) => "!basic".to_string(),
-                AnswerSimplificationType::UnitFactor(true) => "unitFactor".to_string(),
-                AnswerSimplificationType::UnitFactor(false) => "!unitFactor".to_string(),
-                AnswerSimplificationType::UnitPower(true) => "unitPower".to_string(),
-                AnswerSimplificationType::UnitPower(false) => "!unitPower".to_string(),
-                AnswerSimplificationType::UnitDenominator(true) => "unitDenominator".to_string(),
-                AnswerSimplificationType::UnitDenominator(false) => "!unitDenominator".to_string(),
-                AnswerSimplificationType::ZeroFactor(true) => "zeroFactor".to_string(),
-                AnswerSimplificationType::ZeroFactor(false) => "!zeroFactor".to_string(),
-                AnswerSimplificationType::ZeroTerm(true) => "zeroTerm".to_string(),
-                AnswerSimplificationType::ZeroTerm(false) => "!zeroTerm".to_string(),
-                AnswerSimplificationType::ZeroPower(true) => "zeroPower".to_string(),
-                AnswerSimplificationType::ZeroPower(false) => "!zeroPower".to_string(),
-                AnswerSimplificationType::CollectNumbers(true) => "collectNumbers".to_string(),
-                AnswerSimplificationType::CollectNumbers(false) => "!collectNumbers".to_string(),
-                AnswerSimplificationType::ZeroBase(true) => "zeroBase".to_string(),
-                AnswerSimplificationType::ZeroBase(false) => "!zeroBase".to_string(),
-                AnswerSimplificationType::ConstantsFirst(true) => "constantsFirst".to_string(),
-                AnswerSimplificationType::ConstantsFirst(false) => "!constantsFirst".to_string(),
-                AnswerSimplificationType::SqrtProduct(true) => "sqrtProduct".to_string(),
-                AnswerSimplificationType::SqrtProduct(false) => "!sqrtProduct".to_string(),
-                AnswerSimplificationType::SqrtDivision(true) => "sqrtDivision".to_string(),
-                AnswerSimplificationType::SqrtDivision(false) => "!sqrtDivision".to_string(),
-                AnswerSimplificationType::SqrtSquare(true) => "sqrtSquare".to_string(),
-                AnswerSimplificationType::SqrtSquare(false) => "!sqrtSquare".to_string(),
-                AnswerSimplificationType::OtherNumbers(true) => "otherNumbers".to_string(),
-                AnswerSimplificationType::OtherNumbers(false) => "!otherNumbers".to_string(),
-                AnswerSimplificationType::TimesDot(true) => "timesDot".to_string(),
-                AnswerSimplificationType::TimesDot(false) => "!timesDot".to_string(),
-                AnswerSimplificationType::ExpandBrackets(true) => "expandBrackets".to_string(),
-                AnswerSimplificationType::ExpandBrackets(false) => "!expandBrackets".to_string(),
-                AnswerSimplificationType::NoLeadingMinus(true) => "noLeadingMinus".to_string(),
-                AnswerSimplificationType::NoLeadingMinus(false) => "!noLeadingMinus".to_string(),
-                AnswerSimplificationType::Trigonometric(true) => "trig".to_string(),
-                AnswerSimplificationType::Trigonometric(false) => "!trig".to_string(),
-                AnswerSimplificationType::CollectLikeFractions(true) => {
-                    "collectLikeFractions".to_string()
-                }
-                AnswerSimplificationType::CollectLikeFractions(false) => {
-                    "!collectLikeFractions".to_string()
-                }
-                AnswerSimplificationType::CanonicalOrder(true) => "canonicalOrder".to_string(),
-                AnswerSimplificationType::CanonicalOrder(false) => "!canonicalOrder".to_string(),
-                AnswerSimplificationType::CancelFactors(true) => "cancelFactors".to_string(),
-                AnswerSimplificationType::CancelFactors(false) => "!cancelFactors".to_string(),
-                AnswerSimplificationType::CancelTerms(true) => "cancelTerms".to_string(),
-                AnswerSimplificationType::CancelTerms(false) => "!cancelTerms".to_string(),
-                AnswerSimplificationType::Fractions(true) => "simplifyFractions".to_string(),
-                AnswerSimplificationType::Fractions(false) => "!simplifyFractions".to_string(),
-                AnswerSimplificationType::Unknown((n, true)) => n.to_string(),
-                AnswerSimplificationType::Unknown((n, false)) => format!("!{}", n),
-            }
-        )
+        match self {
+            Self::Rule(r) => r.fmt(f),
+            Self::DisplayOption(d) => d.fmt(f),
+        }
     }
 }
 
@@ -120,35 +46,151 @@ impl std::convert::From<AnswerSimplificationType> for String {
     }
 }
 
-#[skip_serializing_none]
-#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
-#[serde(try_from = "&str")]
-#[serde(into = "String")]
-pub enum AnswerSimplificationType {
-    //TODO casing?
-    All(bool),
-    Basic(bool),
-    UnitFactor(bool),
-    UnitPower(bool),
-    UnitDenominator(bool),
-    ZeroFactor(bool),
-    ZeroTerm(bool),
-    ZeroPower(bool),
-    CollectNumbers(bool),
-    ZeroBase(bool),
-    ConstantsFirst(bool),
-    SqrtProduct(bool),
-    SqrtDivision(bool),
-    SqrtSquare(bool),
-    OtherNumbers(bool),
-    TimesDot(bool),
-    ExpandBrackets(bool),
-    CollectLikeFractions(bool),
-    CanonicalOrder(bool),
-    NoLeadingMinus(bool),
-    Fractions(bool),
-    Trigonometric(bool),
-    CancelTerms(bool),
-    CancelFactors(bool),
-    Unknown((String, bool)),
+macro_rules! create_simplification {
+    (
+        $(#[$outer:meta])*
+        pub enum $enum: ident {
+            $(
+                $(#[$inner:meta])*
+                $variant: ident: $name: literal: $lowercase_name: literal
+            ),+
+        }
+    ) => {
+ $(
+            #[$outer]
+        )*
+        #[skip_serializing_none]
+        #[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+        #[serde(try_from = "&str")]
+        #[serde(into = "String")]
+        pub enum $enum {
+            $(
+                $(
+                    #[$inner]
+                )*
+                $variant(bool)
+            ),*
+        }
+
+        impl std::convert::TryFrom<&str> for $enum {
+            type Error = String;
+            fn try_from(whole_item: &str) -> Result<Self, Self::Error> {
+                $(
+                    assert_eq!($lowercase_name.to_string(), $name.to_lowercase());
+                )*
+                let (item, value) = if whole_item.starts_with('!') {
+                    let mut chars = whole_item.chars();
+                    chars.next();
+                    (chars.as_str(), false)
+                } else {
+                    (whole_item, true)
+                };
+                match item.to_lowercase().as_ref() {
+                    $(
+                        $lowercase_name => Ok(Self::$variant(value)),
+                    )*
+                    _ => {
+                        Err(format!("Unknown answer simplification type {}", item))
+                    }
+                }
+            }
+        }
+
+        impl std::fmt::Display for $enum {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(
+                    f,
+                    "{}",
+                    match self {
+                    $(
+                        Self::$variant(true) => $name.to_string(),
+                        Self::$variant(false) => format!("!{}", $name)
+                    ),*
+                    }
+                )
+            }
+        }
+
+        impl std::convert::From<$enum> for String {
+            fn from(a: $enum) -> String {
+                format!("{}", a)
+            }
+        }
+
+
+    };
+}
+
+create_simplification! {
+    /// See https://docs.numbas.org.uk/en/latest/simplification.html#id1 for examples
+    pub enum AnswerSimplificationRule {
+        /// To turn all built-in rules on, use the name all. To turn all built-in rules off, use !all.
+        /// Note: Because they can conflict with other rules, the canonicalOrder and expandBrackets rules are not included in all. You must include them separately.
+        All: "all": "all",
+        /// Basic simplifications like unary plus removal etc
+        Basic: "basic": "basic",
+        /// Cancel products of 1
+        CancelUnitFactor: "unitFactor": "unitfactor",
+        /// Cancel exponents of 1
+        CancelUnitPower: "unitPower": "unitpower",
+        /// Cancel fractions with denominator 1
+        CancelUnitDenominator: "unitDenominator": "unitdenominator",
+        /// Cancel products of zero to zero
+        CancelZeroFactor: "zeroFactor": "zerofactor",
+        /// Omit zero terms
+        OmitZeroTerm: "zeroTerm": "zeroterm",
+        /// Cancel exponents of 0
+        CancelZeroPower: "zeroPower" : "zeropower",
+        /// Rearrange expressions so they don’t start with a unary minus
+        NoLeadingMinus: "noLeadingMinus": "noleadingminus",
+        /// Collect together numerical (as opposed to variable) products and sums.
+        CollectNumbers: "collectNumbers": "collectnumbers",
+        /// Cancel fractions to lowest form.
+        Fractions: "simplifyFractions": "simplifyfractions",
+        /// Cancel any power of zero
+        CancelZeroBase: "zeroBase": "zerobase",
+        /// Numbers go to the left of multiplications
+        ConstantsFirst: "constantsFirst": "constantsfirst",
+        /// Collect products of square roots
+        CollectSqrtProducts: "sqrtProduct": "sqrtproduct",
+        /// Collect fractions of square roots
+        CollectSqrtDivisions: "sqrtDivision": "sqrtdivision",
+        /// Cancel square roots of squares, and squares of square roots
+        CancelSqrtSquare: "sqrtSquare": "sqrtsquare",
+        /// Simplify some trigonometric identities
+        Trigonometric: "trig": "trig",
+        /// Evaluate powers of numbers.
+        EvaluatePowersOfNumbers: "otherNumbers": "othernumbers",
+        /// Collect together and cancel terms. Like collectNumbers, but for any kind of term.
+        CollectTerms: "cancelTerms": "cancelterms",
+        /// Collect together powers of common factors.
+        CollectPowersOfCommonFactors: "cancelFactors": "cancelfactors",
+        /// Collect together fractions over the same denominator.
+        CollectLikeFractions: "collectLikeFractions": "collectlikefractions",
+        /// earrange the expression into a “canonical” order, using canonical_compare().
+        /// Note: This rule can not be used at the same time as noLeadingMinus - it can lead to an infinite loop.
+        CanonicalOrder: "canonicalOrder": "canonicalorder",
+        /// Expand out products of sums. (distributivity)
+        ExpandBrackets: "expandBrackets": "expandbrackets"
+    }
+}
+
+create_simplification! {
+    /// Se https://docs.numbas.org.uk/en/latest/simplification.html#display-options for examples
+    pub enum AnswerSimplificationDisplayOption {
+        /// This rule doesn’t rewrite expressions, but tells the maths renderer that you’d like non-integer numbers to be displayed as fractions instead of decimals.
+        Fractions: "fractionNumbers": "fractionnumbers",
+        /// Improper fractions (with numerator larger than the denominator) are displayed in mixed form, as an integer next to a proper fraction.
+        MixedFractions: "mixedFractions": "mixedfractions",
+        /// Fractions are displayed on a single line, with a slash between the numerator and denominator.
+        FlatFractions: "flatFractions": "flatfractions",
+        /// This rule doesn’t rewrite expressions, but tells the maths renderer that you’d like vectors to be rendered as rows instead of columns.
+        RowVector: "rowVector": "rowvector",
+        /// The multiplication symbol is always included between multiplicands.
+        AlwaysShowMultiplicationSign: "alwaysTimes": "alwaystimes",
+        /// Use a dot for the multiplication symbol instead of a cross.
+        DotAsMultiplicationSign: "timesDot": "timesdot",
+        /// Matrices are rendered without parentheses.
+        MatricesWithoutParentheses: "bareMatrices" : "barematrices"
+    }
 }
