be17b8bce7525af3389d3d9c85a1147fb922ed0e
Jesse Hoobergs <jhoobergs@users.noreply.github.com>
2021-12-07 11:53:36 +0100

    Create rumbas types for CustomPartTypeSetting numbas type

--- rumbas/src/question/custom_part_type.rs
+++ rumbas/src/question/custom_part_type.rs
@@ -1,11 +1,12 @@
 use crate::question::extension::Extensions;
 use crate::question::part::question_part::JMENotes;
+use crate::support::noneable::Noneable;
 use crate::support::to_numbas::ToNumbas;
 use crate::support::to_rumbas::ToRumbas;
+use crate::support::translatable::EmbracedJMETranslatableString;
 use crate::support::translatable::JMETranslatableString;
 use crate::support::translatable::TranslatableString;
 use crate::support::yaml::{YamlError, YamlResult};
-use numbas::question::custom_part_type::CustomPartTypeSetting as NCustomPartTypeSetting;
 use rumbas_support::preamble::*;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -18,7 +19,7 @@ use std::hash::{Hash, Hasher};
 pub struct CustomPartTypeDefinition {
     pub type_name: TranslatableString,
     pub description: TranslatableString,
-    pub settings: Vec<NCustomPartTypeSetting>, // TODO
+    pub settings: Vec<CustomPartTypeSetting>,
     pub can_be_gap: bool,
     pub can_be_step: bool,
     pub marking_notes: JMENotes,
@@ -44,7 +45,7 @@ impl ToNumbas<numbas::question::custom_part_type::CustomPartType> for CustomPart
             short_name: name,
             name: self.type_name.clone().to_string(locale).unwrap(),
             description: self.description.clone().to_string(locale).unwrap(),
-            settings: self.settings.clone(), // .to_numbas(&locale).unwrap(),
+            settings: self.settings.to_numbas(&locale),
             help_url: self.help_url.clone().to_string(locale).unwrap(),
             public_availability: numbas::question::custom_part_type::CustomPartAvailability::Always,
             marking_script: self.marking_notes.to_numbas(locale),
@@ -81,9 +82,401 @@ impl CustomPartTypeDefinition {
     }
 }
 
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+#[serde(tag = "input_type")]
+#[serde(rename_all = "snake_case")]
+pub enum CustomPartTypeSetting {
+    CheckBox(CustomPartTypeSettingCheckBox),
+    Code(CustomPartTypeSettingCode),
+    MathematicalExpression(CustomPartTypeSettingMathematicalExpression),
+    String(CustomPartTypeSettingString),
+    DropDown(CustomPartTypeSettingDropDown),
+    Percentage(CustomPartTypeSettingPercentage),
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSetting> for CustomPartTypeSetting {
+    fn to_numbas(&self, locale: &str) -> numbas::question::custom_part_type::CustomPartTypeSetting {
+        match self {
+            Self::CheckBox(c) => {
+                numbas::question::custom_part_type::CustomPartTypeSetting::CheckBox(
+                    c.to_numbas(&locale),
+                )
+            }
+            Self::Code(c) => numbas::question::custom_part_type::CustomPartTypeSetting::Code(
+                c.to_numbas(&locale),
+            ),
+            Self::MathematicalExpression(c) => {
+                numbas::question::custom_part_type::CustomPartTypeSetting::MathematicalExpression(
+                    c.to_numbas(&locale),
+                )
+            }
+            Self::String(c) => numbas::question::custom_part_type::CustomPartTypeSetting::String(
+                c.to_numbas(&locale),
+            ),
+            Self::DropDown(c) => {
+                numbas::question::custom_part_type::CustomPartTypeSetting::DropDown(
+                    c.to_numbas(&locale),
+                )
+            }
+            Self::Percentage(c) => {
+                numbas::question::custom_part_type::CustomPartTypeSetting::Percentage(
+                    c.to_numbas(&locale),
+                )
+            }
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSetting> for numbas::question::custom_part_type::CustomPartTypeSetting {
+    fn to_rumbas(&self) -> CustomPartTypeSetting {
+        match self {
+            Self::CheckBox(c) => CustomPartTypeSetting::CheckBox(c.to_rumbas()),
+            Self::Code(c) => CustomPartTypeSetting::Code(c.to_rumbas()),
+            Self::MathematicalExpression(c) => {
+                CustomPartTypeSetting::MathematicalExpression(c.to_rumbas())
+            }
+            Self::String(c) => CustomPartTypeSetting::String(c.to_rumbas()),
+            Self::DropDown(c) => CustomPartTypeSetting::DropDown(c.to_rumbas()),
+            Self::Percentage(c) => CustomPartTypeSetting::Percentage(c.to_rumbas()),
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingSharedDataInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingSharedData {
+    /// A short name for this setting, used to refer to it in the part type’s answer input or marking algorithm. The name should uniquely identify the setting.
+    name: TranslatableString,
+    /// The label shown next to the setting in the numbas question editor. Try to make it as clear as possible what the setting is for. For example, a checkbox which dictates whether an input hint is shown should be labelled something like “Hide the input hint?” rather than “Input hint visibility” - the latter doesn’t tell the question author whether ticking the checkbox will result in the input hint appearing or not.
+    numbas_label: TranslatableString,
+    /// The address of documentation explaining this setting in further depth.
+    documentation_url: Noneable<TranslatableString>,
+    /// Use this field to give further guidance to question authors about this setting, if the label is not enough. For example, you might use this to say what data type a JME code setting should evaluate to.
+    numbas_hint: TranslatableString,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingSharedData>
+    for CustomPartTypeSettingSharedData
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingSharedData {
+        numbas::question::custom_part_type::CustomPartTypeSettingSharedData {
+            name: self.name.to_numbas(locale),
+            label: self.numbas_label.to_numbas(locale),
+            hint: self.numbas_hint.to_numbas(locale),
+            help_url: self.documentation_url.to_numbas(locale),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingSharedData>
+    for numbas::question::custom_part_type::CustomPartTypeSettingSharedData
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingSharedData {
+        CustomPartTypeSettingSharedData {
+            name: self.name.to_rumbas(),
+            numbas_label: self.label.to_rumbas(),
+            numbas_hint: self.hint.to_rumbas(),
+            documentation_url: self.help_url.to_rumbas(),
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingStringInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingString {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    /// If this is ticked, then JME expressions enclosed in curly braces will be evaluated and the results substituted back into the text when the question is run. Otherwise, the string will be untouched.
+    evaluate_enclosed_expressions: bool,
+    /// The initial value of the setting in the question editor. If the setting has a sensible default value, set it here. If the value of the setting is likely to be different for each instance of this part type, set this to none.
+    default_value: Noneable<String>,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingString>
+    for CustomPartTypeSettingString
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingString {
+        numbas::question::custom_part_type::CustomPartTypeSettingString {
+            shared_data: self.shared_data.to_numbas(locale),
+            evaluate_enclosed_expressions: self.evaluate_enclosed_expressions.to_numbas(locale),
+            default_value: self
+                .default_value
+                .to_numbas(locale)
+                .unwrap_or_else(String::new), // TODO implement String to Noneable<String> where it is None if string is empty
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingString>
+    for numbas::question::custom_part_type::CustomPartTypeSettingString
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingString {
+        CustomPartTypeSettingString {
+            shared_data: self.shared_data.to_rumbas(),
+            evaluate_enclosed_expressions: self.evaluate_enclosed_expressions.to_rumbas(),
+            default_value: if self.default_value.is_empty() {
+                Noneable::None
+            } else {
+                Noneable::NotNone(self.default_value.to_rumbas())
+            },
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingMathematicalExpressionInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingMathematicalExpression {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    ///  If this is ticked, then JME expressions enclosed in curly braces will be evaluated and the results substituted back into the string.
+    evaluate_enclosed_expressions: bool,
+    /// The initial value of the setting in the question editor. If the setting has a sensible default value, set it here. If the value of the setting is likely to be different for each instance of this part type, set this to none.
+    default_value: Noneable<EmbracedJMETranslatableString>,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingMathematicalExpression>
+    for CustomPartTypeSettingMathematicalExpression
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingMathematicalExpression {
+        numbas::question::custom_part_type::CustomPartTypeSettingMathematicalExpression {
+            shared_data: self.shared_data.to_numbas(locale),
+            evaluate_enclosed_expressions: self.evaluate_enclosed_expressions.to_numbas(locale),
+            default_value: self.default_value.to_numbas(locale).unwrap_or_default(),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingMathematicalExpression>
+    for numbas::question::custom_part_type::CustomPartTypeSettingMathematicalExpression
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingMathematicalExpression {
+        CustomPartTypeSettingMathematicalExpression {
+            shared_data: self.shared_data.to_rumbas(),
+            evaluate_enclosed_expressions: self.evaluate_enclosed_expressions.to_rumbas(),
+            default_value: if self.default_value.is_empty() {
+                Noneable::None
+            } else {
+                Noneable::NotNone(self.default_value.to_rumbas())
+            },
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingCodeInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingCode {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    /// The initial value of the setting in the question editor. If the setting has a sensible default value, set it here. If the value of the setting is likely to be different for each instance of this part type, set this to none.
+    default_value: Noneable<JMETranslatableString>,
+    evaluate: bool,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingCode>
+    for CustomPartTypeSettingCode
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingCode {
+        numbas::question::custom_part_type::CustomPartTypeSettingCode {
+            shared_data: self.shared_data.to_numbas(locale),
+            evaluate: self.evaluate.to_numbas(locale),
+            default_value: self.default_value.to_numbas(locale).unwrap_or_default(),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingCode>
+    for numbas::question::custom_part_type::CustomPartTypeSettingCode
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingCode {
+        CustomPartTypeSettingCode {
+            shared_data: self.shared_data.to_rumbas(),
+            evaluate: self.evaluate.to_rumbas(),
+            default_value: if self.default_value.is_empty() {
+                Noneable::None
+            } else {
+                Noneable::NotNone(self.default_value.to_rumbas())
+            },
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingCheckBoxInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingCheckBox {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    /// The initial value of the setting in the question editor.
+    default_value: bool,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingCheckBox>
+    for CustomPartTypeSettingCheckBox
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingCheckBox {
+        numbas::question::custom_part_type::CustomPartTypeSettingCheckBox {
+            shared_data: self.shared_data.to_numbas(locale),
+            default_value: self.default_value.to_numbas(locale),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingCheckBox>
+    for numbas::question::custom_part_type::CustomPartTypeSettingCheckBox
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingCheckBox {
+        CustomPartTypeSettingCheckBox {
+            shared_data: self.shared_data.to_rumbas(),
+            default_value: self.default_value.to_rumbas(),
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingDropDownInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingDropDown {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    /// The initial value of the setting in the question editor. If the setting has a sensible default value, set it here. If the value of the setting is likely to be different for each instance of this part type, set this to none.
+    default_value: Noneable<TranslatableString>,
+    choices: Vec<CustomPartTypeSettingDropDownChoice>,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingDropDown>
+    for CustomPartTypeSettingDropDown
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingDropDown {
+        numbas::question::custom_part_type::CustomPartTypeSettingDropDown {
+            shared_data: self.shared_data.to_numbas(locale),
+            default_value: self
+                .default_value
+                .to_numbas(locale)
+                .unwrap_or_else(String::new)
+                .into(),
+            choices: self.choices.to_numbas(locale),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingDropDown>
+    for numbas::question::custom_part_type::CustomPartTypeSettingDropDown
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingDropDown {
+        CustomPartTypeSettingDropDown {
+            shared_data: self.shared_data.to_rumbas(),
+            default_value: if self.default_value.to_string().is_empty() {
+                Noneable::None
+            } else {
+                Noneable::NotNone(self.default_value.to_string().to_rumbas())
+            },
+            choices: self.choices.to_rumbas(),
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingDropDownChoiceInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingDropDownChoice {
+    value: TranslatableString,
+    label: TranslatableString,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingDropDownChoice>
+    for CustomPartTypeSettingDropDownChoice
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingDropDownChoice {
+        numbas::question::custom_part_type::CustomPartTypeSettingDropDownChoice {
+            value: self.value.to_numbas(locale).into(),
+            label: self.label.to_numbas(locale).into(),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingDropDownChoice>
+    for numbas::question::custom_part_type::CustomPartTypeSettingDropDownChoice
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingDropDownChoice {
+        CustomPartTypeSettingDropDownChoice {
+            value: self.value.to_string().into(),
+            label: self.label.to_string().into(),
+        }
+    }
+}
+
+#[derive(Input, Overwrite, RumbasCheck, Examples)]
+#[input(name = "CustomPartTypeSettingPercentageInput")]
+#[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
+pub struct CustomPartTypeSettingPercentage {
+    #[serde(flatten)]
+    shared_data: CustomPartTypeSettingSharedData,
+    /// The initial value of the setting in the question editor. If the setting has a sensible default value, set it here. If the value of the setting is likely to be different for each instance of this part type, set this to none.
+    default_value: Noneable<f64>,
+}
+
+impl ToNumbas<numbas::question::custom_part_type::CustomPartTypeSettingPercentage>
+    for CustomPartTypeSettingPercentage
+{
+    fn to_numbas(
+        &self,
+        locale: &str,
+    ) -> numbas::question::custom_part_type::CustomPartTypeSettingPercentage {
+        numbas::question::custom_part_type::CustomPartTypeSettingPercentage {
+            shared_data: self.shared_data.to_numbas(locale),
+            default_value: self
+                .default_value
+                .map(|n| n.into())
+                .unwrap_or_else(|| String::new().into()),
+        }
+    }
+}
+
+impl ToRumbas<CustomPartTypeSettingPercentage>
+    for numbas::question::custom_part_type::CustomPartTypeSettingPercentage
+{
+    fn to_rumbas(&self) -> CustomPartTypeSettingPercentage {
+        CustomPartTypeSettingPercentage {
+            shared_data: self.shared_data.to_rumbas(),
+            default_value: match self.default_value {
+                numbas::support::primitive::Primitive::Float(f) => Noneable::NotNone(f),
+                numbas::support::primitive::Primitive::Natural(f) => Noneable::NotNone(f as f64),
+                numbas::support::primitive::Primitive::String(s) => Noneable::None,
+            },
+        }
+    }
+}
+
 macro_rules! create_input_option_value {
     ($struct: ident, $input: literal, $type: ty, $numbas_subtype: ty) => {
-        #[derive(Input, Overwrite, RumbasCheck)]
+        #[derive(Input, Overwrite, RumbasCheck, Examples)]
         #[input(name = $input)]
         #[derive(Serialize, Deserialize, JsonSchema, Debug, Clone, PartialEq)]
         pub struct $struct {
@@ -367,7 +760,7 @@ impl ToRumbas<CustomPartTypeDefinitionPath> for numbas::question::custom_part_ty
             custom_part_type_data: CustomPartTypeDefinition {
                 type_name: self.name.to_rumbas(),
                 description: self.description.to_rumbas(),
-                settings: self.settings.clone(),
+                settings: self.settings.to_rumbas(),
                 help_url: self.help_url.to_rumbas(),
                 // public_availability: numbas::question::custom_part_type::CustomPartAvailability::Always,
                 can_be_gap: self.can_be_gap,
