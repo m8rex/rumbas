3fd11202e9b64e7e2a75c2cf94e057aef9d59af0
Jesse Hoobergs <jhoobergs@users.noreply.github.com>
2021-12-21 15:19:00 +0100

    Improve zero_four_zero.rs

--- rumbas/src/updates/zero_four_zero.rs
+++ rumbas/src/updates/zero_four_zero.rs
@@ -4,37 +4,79 @@ use yaml_rust::{yaml::Yaml, YamlEmitter, YamlLoader};
 /// Update from version 0.4.0 to 0.5.0
 pub fn update() -> String {
     // TODO: extract code to read questions and exams
-    let question_files = CACHE
+    let question_files: Vec<_> = CACHE
         .read_all_questions()
         .into_iter()
         .filter_map(|lf| match lf {
             rumbas_support::input::LoadedFile::Normal(n) => Some(n),
             _ => None,
-        });
-    let exam_files = CACHE
+        })
+        .collect();
+    let exam_files: Vec<_> = CACHE
         .read_all_exams()
         .into_iter()
         .filter_map(|lf| match lf {
             rumbas_support::input::LoadedFile::Normal(n) => Some(n),
             _ => None,
-        });
+        })
+        .collect();
 
-    let questions: Vec<_> = question_files
+    let mut questions: Vec<_> = question_files
         .into_iter()
-        .filter_map(|lf| YamlLoader::load_from_str(&lf.content[..]).ok())
-        .map(|ys| ys[0].clone())
+        .filter_map(|lf| {
+            YamlLoader::load_from_str(&lf.content[..])
+                .ok()
+                .map(|a| (lf, a[0].clone()))
+        })
         .collect();
 
     let exams: Vec<_> = exam_files
         .into_iter()
-        .filter_map(|lf| YamlLoader::load_from_str(&lf.content[..]).ok())
-        .map(|ys| ys[0].clone())
+        .filter_map(|lf| {
+            YamlLoader::load_from_str(&lf.content[..])
+                .map(|a| (lf, a[0].clone()))
+                .ok()
+        })
         .collect();
+
+    for question_idx in 0..questions.len() {
+        let question = &questions[question_idx];
+        log::info!("Updating {}", question.0.file_path.display());
+        let new_question = question
+            .1
+            .as_hash()
+            .unwrap()
+            .to_owned()
+            .into_iter()
+            .map(|(k, v)| {
+                if vec!["advice", "statement"]
+                    .into_iter()
+                    .map(|a| Yaml::String(a.to_string()))
+                    .collect::<Vec<_>>()
+                    .contains(&k)
+                {
+                    (k, update_translatable_string(v))
+                } else {
+                    (k, v)
+                }
+            })
+            .collect();
+        questions[question_idx].1 = Yaml::Hash(new_question);
+    }
+
+    for (file, question) in questions.into_iter() {
+        let mut out_str = String::new();
+        {
+            let mut emitter = YamlEmitter::new(&mut out_str);
+            emitter.dump(&question).unwrap(); // dump the YAML object to a String
+        }
+        std::fs::write(file.file_path, out_str).expect("Failed writing file");
+    }
 
     "0.5.0".to_string()
 }
 
-fn update_translatable_string(yaml: &mut Yaml) {
+fn update_translatable_string(yaml: Yaml) -> Yaml {
     match yaml {
         Yaml::Hash(h) => {
             let values: Vec<_> = h
@@ -68,7 +110,7 @@ fn update_translatable_string(yaml: &mut Yaml) {
                 .map(|(p, v)| {
                     (
                         Yaml::String(remove_first_and_last(&p[..]).to_string()),
-                        v.clone(),
+                        update_translatable_string_placeholder(v.clone()),
                     )
                 })
                 .collect();
@@ -84,9 +126,24 @@ fn update_translatable_string(yaml: &mut Yaml) {
                 .into_iter()
                 .collect(),
             );
-            *yaml = new_yaml;
+            new_yaml
         }
-        _ => (),
+        _ => yaml,
+    }
+}
+
+fn update_translatable_string_placeholder(yaml: Yaml) -> Yaml {
+    match yaml {
+        Yaml::Hash(_) => update_translatable_string(yaml),
+        Yaml::String(_) => Yaml::Hash(
+            vec![
+                (Yaml::String("content".to_string()), yaml),
+                (Yaml::String("placeholders".to_string()), Yaml::Null),
+            ]
+            .into_iter()
+            .collect(),
+        ),
+        _ => yaml,
     }
 }
 
