daf793751bac1597e1b8467dfad5062f8d5fb89e
Jesse Hoobergs <jhoobergs@users.noreply.github.com>
2021-12-28 01:49:59 +0100

    Allow passing a folder path to the check command

--- rumbas/src/cli/check.rs
+++ rumbas/src/cli/check.rs
@@ -1,3 +1,4 @@
+use rumbas::support::file_manager::CACHE;
 use rumbas::support::to_numbas::ToNumbas;
 use rumbas_support::preamble::Input;
 use std::path::Path;
@@ -10,6 +11,39 @@ pub fn check(matches: &clap::ArgMatches) {
         return;
     }
 
+    let files = if path.is_file() {
+        vec![path.to_path_buf()]
+    } else if path.is_dir() {
+        if path.starts_with("question") {
+            CACHE.find_all_questions_in_folder(path.to_path_buf())
+        } else {
+            CACHE.find_all_exams_in_folder(path.to_path_buf())
+        }
+        .into_iter()
+        .map(|f| f.file_path)
+        .collect()
+    } else {
+        log::error!("Symbolic links are not (yet) supported");
+        std::process::exit(1);
+    };
+    let nb_failures = files.iter().fold(0, |nb, file| {
+        if check_file(matches, &file) {
+            nb
+        } else {
+            nb + 1
+        }
+    });
+    if nb_failures > 0 {
+        log::error!("{} files failed.", nb_failures);
+        std::process::exit(1);
+    } else {
+        log::info!("All checks passed.")
+    }
+}
+
+/// Return true if parsing is ok
+fn check_file(_matches: &clap::ArgMatches, path: &Path) -> bool {
+    log::info!("Compiling {:?}", path.display());
     let exam_input_result = rumbas::exam::ExamInput::from_file(path);
     match exam_input_result {
         Ok(mut exam_input) => {
@@ -19,8 +53,8 @@ pub fn check(matches: &clap::ArgMatches) {
             match exam_result {
                 Ok(exam) => {
                     if exam.locales().is_empty() {
-                        log::error!("Locales not set!");
-                        std::process::exit(1)
+                        log::error!("Locales not set for {}!", path.display());
+                        return false;
                     } else {
                         let mut something_failed: bool = false;
                         for locale_item in exam.locales().iter() {
@@ -30,25 +64,28 @@ pub fn check(matches: &clap::ArgMatches) {
                                 Ok(_) => {}
                                 Err(check_result) => {
                                     something_failed = true;
-                                    log::error!("Error when processing locale {}.", locale);
+                                    log::error!(
+                                        "Error when processing locale {} for {}.",
+                                        locale,
+                                        path.display()
+                                    );
                                     check_result.log();
                                 }
                             }
                         }
-                        if something_failed {
-                            std::process::exit(1)
-                        }
+                        return !something_failed;
                     }
                 }
                 Err(check_result) => {
+                    log::info!("Failed compiling {:?}", path.display());
                     check_result.log();
-                    std::process::exit(1)
+                    return false;
                 }
             }
         }
         Err(e) => {
             log::error!("{}", e);
-            std::process::exit(1)
+            return false;
         }
     };
 }
