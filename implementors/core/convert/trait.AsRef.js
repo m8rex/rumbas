9860ffa15554c993e8b2593968bf9f5b03e354c9
jhoobergs <jhoobergs@users.noreply.github.com>
2022-09-26 09:28:05 +0000

    Deploying to gh-pages from  @ ba8878cb10c14e14c1dc945519fc0ad47b2929ff 🚀

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rumbas_support":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/path/struct.Path.html\" title=\"struct std::path::Path\">Path</a>&gt; for <a class=\"struct\" href=\"rumbas_support/path/struct.RumbasPath.html\" title=\"struct rumbas_support::path::RumbasPath\">RumbasPath</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rumbas_support/sidebar-items.js
+++ rumbas_support/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["example",""],["input",""],["overwrite",""],["preamble",""],["rumbas_check",""],["value",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["example",""],["input",""],["overwrite",""],["path",""],["preamble",""],["rumbas_check",""],["value",""]]};
