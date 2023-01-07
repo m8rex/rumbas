c9814a3291749df87b016dae21cf1dd51801c722
Jesse Hoobergs <jhoobergs@users.noreply.github.com>
2023-01-07 21:58:04 +0100

    Update code, almost done

--- rumbas/src/updates/zero_seven_one.rs
+++ rumbas/src/updates/zero_seven_one.rs
@@ -4,8 +4,6 @@ use yaml_subset::{parse_yaml_file, AliasedYaml, Yaml, YamlInsert, YamlPath};
 
 pub fn update() -> semver::Version {
     if let Some(root) = within_repo(Path::new(".")) {
-        let default_files = super::find_default_files(&root);
-
         // Update exam files
         let mut exams = super::read_all_exams(&root);
         for exam in &mut exams {
@@ -24,22 +22,97 @@ pub fn update() -> semver::Version {
             );
         }
         super::write_files(exams);
+
+        // Update question files
+        let default_files = super::find_default_files(&root);
+        // TODO: default_files
+
+        let mut questions = super::read_all_questions(&root);
+        for question in &mut questions {
+            log::info!("Fixing {}", question.0.file_path.display());
+            // Remove `minimum_marks` field
+            question
+                .1
+                .remove_from_hash(&"parts[*].minimum_marks".parse().unwrap());
+            // Remove `enable_minimum_marks` field
+            question
+                .1
+                .remove_from_hash(&"parts[*].enable_minimum_marks".parse().unwrap());
+            // Remove `use_custom_name`
+            question
+                .1
+                .remove_from_hash(&"parts[*].use_custom_name".parse().unwrap());
+            // Rename `custom_name` -> `part_name` (this fields can now be set to 'none')
+            question.1.rename_field(
+                &"parts[*].custom_name".parse().unwrap(),
+                "part_name".to_string(),
+            );
+
+            // Move following fields (and rename them) to a new field named `custom_marking` (which can be set to 'none')
+            // custom_marking_algorithm -> custom_marking.algorithm_notes
+            // extend_base_marking_algorithm -> custom_marking.extend_base_marking_algorithm
+            question.1.move_to_subfield(
+                &"parts[*]".parse().unwrap(),
+                "custom_marking".to_string(),
+                vec![
+                    "custom_marking_algorithm".to_string(),
+                    "extend_base_marking_algorithm".to_string(),
+                ],
+            );
+            question.1.rename_field(
+                &"parts[*].custom_marking.custom_marking_algorithm"
+                    .parse()
+                    .unwrap(),
+                "algorithm_notes".to_string(),
+            );
+
+            // Move following fields (and rename them) to a new field named `adaptive_marking` (which can be set to 'none')
+            // variable_replacement_strategy -> adaptive_marking.variable_replacement_strategy
+            // adaptive_marking_penalty -> adaptive_marking.penalty
+            // Also add a new field adaptive_marking.variable_replacements
+            question.1.move_to_subfield(
+                &"parts[*]".parse().unwrap(),
+                "adaptive_marking".to_string(),
+                vec![
+                    "variable_replacement_strategy".to_string(),
+                    "adaptive_marking_penalty".to_string(),
+                ],
+            );
+            question.1.rename_field(
+                &"parts[*].adaptive_marking.adaptive_marking_penalty"
+                    .parse()
+                    .unwrap(),
+                "penalty".to_string(),
+            );
+            question.1.insert_into_hash(
+                &"parts[*].adaptive_marking.variable_replacements"
+                    .parse()
+                    .unwrap(),
+                &AliasedYaml {
+                    alias: None,
+                    value: Yaml::InlineArray(Vec::new()),
+                },
+                false,
+            );
+
+            // NumberEntry question parts
+            // Rename hint_fraction to fractions_must_be_reduced_hint"
+            question.1.rename_field(
+                &"parts[*].hint_fraction|type=number_entry".parse().unwrap(),
+                "fractions_must_be_reduced_hint".to_string(),
+            );
+
+            // Pattern match question parts
+            // rename partial_credit to wrong_case_partial_credit
+            question.1.rename_field(
+                &"parts[*].partial_credit|type=pattern_match"
+                    .parse()
+                    .unwrap(),
+                "wrong_case_partial_credit".to_string(),
+            );
+        }
+        super::write_files(questions);
     }
-
-    log::warn!("");
-    log::warn!("Question parts:");
-    log::warn!("Remove `minimum_marks` field");
-    log::warn!("Remove `enable_minimum_marks` field");
-    log::warn!("Remove `use_custom_name` field");
-    log::warn!("Rename `custom_name` -> `part_name` (this fields can now be set to 'none')");
-    log::warn!("Move following fields (and rename them) to a new field named `adaptive_marking` (which can be set to 'none')");
-    log::warn!("variable_replacement_strategy -> adaptive_marking.variable_replacement_strategy");
-    log::warn!("adaptive_marking_penalty -> adaptive_marking.penalty");
-    log::warn!("Also add a new field adaptive_marking.variable_replacements");
-
-    log::warn!("Move following fields (and rename them) to a new field named `custom_marking` (which can be set to 'none')");
-    log::warn!("custom_marking_algorithm -> custom_marking.algorithm_notes");
-    log::warn!("extend_base_marking_algorithm -> custom_marking.extend_base_marking_algorithm");
 
     log::warn!("");
     log::warn!("JME Question parts:");
@@ -48,14 +121,6 @@ pub fn update() -> semver::Version {
     log::warn!("failure_rate -> accuracy.max_failures");
     log::warn!("vset_range -> accuracy.checking_range");
     log::warn!("vset_range_point -> accuracy.points_to_check");
-
-    log::warn!("");
-    log::warn!("NumberEntry Question parts:");
-    log::warn!("hint_fraction -> fractions_must_be_reduced_hint");
-
-    log::warn!("");
-    log::warn!("Pattern Match Question parts:");
-    log::warn!("partial_credit -> wrong_case_partial_credit");
 
     semver::Version::new(0, 8, 0)
 }
